0c76531e354b8c2099f4b1fa882d684a675552d1
Jhicken16 <jhicken16@gmail.com>
2023-12-13 17:47:34 +0000

    database pool set up test route added to server.js. pg added to packages, config expanded to inclued db paramitors.

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-const express = require('express')
+const express = require('express');
 
-const { PORT } = require('./config')
+const { PORT } = require('./config');
+const db = require('./db/index')
  
-const app = express()
+const app = express();
+
+//This was to test that database was set up correctly.
+app.get('/', async (req, res) => {
+  try {
+    const result = await db.query('SELECT * FROM customers');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`)
-})
+    console.log(`Server is listening on port ${PORT}`);
+});
